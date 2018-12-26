0831fe88591f30926d67248eab4da081815db924
montoyaguzman <montoyaguzman7@gmail.com>
2018-12-26 17:34:54 -0600

    agrega ejemplo de arrows

---
+++ arrows/arrows.js
@@ -0,0 +1,11 @@
+const nombre = function(nombre) {
+  console.log('Saludando a ', nombre)
+}
+
+const nombre2 = (nombre) => {
+  console.log('Saludando a ', nombre)
+}
+
+miNombre = 'Jose'
+nombre(miNombre)
+nombre2(miNombre)
